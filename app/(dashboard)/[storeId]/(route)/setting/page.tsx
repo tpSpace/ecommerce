3b72595e9794ff13eba0e5bf0434e7c17538b8ec
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2024-06-05 11:39:40 +0700

    init setting page

---
+++ app/(dashboard)/[storeId]/(route)/setting/page.tsx
@@ -0,0 +1,36 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+interface SettingsPageProps {
+    params: {
+        storeId: string;
+    }
+}
+
+const SettingsPage: React.FC<SettingsPageProps> = async ({ params }) => {
+    const { userId } = auth();
+
+    if (!userId) {
+        redirect("/sign-in");
+    }
+
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId,
+            userId
+        }
+    });
+
+    if (!store) {
+        redirect("/");
+    }
+
+    return (
+        <div>
+            Setting Page
+        </div>
+    )
+}
+
+export default SettingsPage;
