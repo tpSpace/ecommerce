920b09a2646afa5613309684bc44ed0b2c026087
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2024-06-06 20:50:25 +0700

    Remove redeundant libs

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -23,10 +23,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { useParams, useRouter } from "next/navigation";
 import { AlertModal } from "@/components/modals/alert-modal";
-import { ApiAlert } from "@/components/ui/api-alert";
-import { useOrigin } from "@/hooks/use-origin";
 import ImageUpload from "@/components/ui/image-upload";
-import { init } from "next/dist/compiled/webpack/webpack";
 
 interface BillboardFormProps {
   initialData: Billboard | null;
