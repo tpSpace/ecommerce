255e01fbcb66da766750a5236c0fbebf847cbe66
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2024-06-06 20:51:40 +0700

    Adjust nav bar components position

--- components/Main-nav.tsx
+++ components/Main-nav.tsx
@@ -18,14 +18,14 @@ export function MainNav({
       active: pathname === `/${params.storeId}`,
     },
     {
-      href: `/${params.storeId}/settings`,
-      label: "Settings",
-      active: pathname === `/${params.storeId}/settings`,
-    },
-    {
       href: `/${params.storeId}/billboards`,
       label: "Billboard",
       active: pathname === `/${params.storeId}/billboards`,
+    },
+    {
+      href: `/${params.storeId}/settings`,
+      label: "Settings",
+      active: pathname === `/${params.storeId}/settings`,
     },
   ];
 
