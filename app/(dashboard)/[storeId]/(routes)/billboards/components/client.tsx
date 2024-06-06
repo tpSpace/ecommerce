cac0c52b2953f46f6c86dfd920fef1ea2fc20603
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2024-06-06 22:38:08 +0700

    Fix billboard url && Add data-table

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -8,25 +8,33 @@ import { Plus } from "lucide-react";
 import { Separator } from "@radix-ui/react-separator";
 
 import { useParams, useRouter } from "next/navigation";
+import { BillboardColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
 
-export const BillboardClient = () => {
+interface BillboardClientProps {
+  data: BillboardColumn[];
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
 
   return (
     <>
       <div className="flex items-center justify-between">
-        <Heading title="Billboards (0)" description="Manage your billboards" />
+        <Heading
+          title={`Billboards ${data.length}`}
+          description="Manage your billboards"
+        />
         <Button
-          onClick={() =>
-            router.push(`/dashboard/${params.storeId}/billboards/new`)
-          }
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
         >
           <Plus className="mr-2 h-4 w-4" />
           Add new
         </Button>
       </div>
       <Separator />
+      <DataTable columns={columns} data={data} searchKey="label" />
     </>
   );
 };
