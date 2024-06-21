a9f96978a198d4c36711c2cab917ca4513fb682c
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2024-06-21 07:30:32 +0700

    Edit middleware

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,9 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // const isProtectedRoute = createRouteMatcher(["/"]);
 
-const isPublicRoute = createRouteMatcher(["/api/:path*"]);
-
-export default clerkMiddleware(
-  (auth, req) => {
-    if (!isPublicRoute(req)) auth().protect();
-  },
-  // // { debug: true }
-);
+export default clerkMiddleware({
+  publicRoutes: ["/api/:path*"],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
